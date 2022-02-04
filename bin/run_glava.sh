b5f6e47796c0b36b5dc5d8fb88e7e06005f42a12
Federico Galatolo <galatolo.federico@gmail.com>
2022-02-04 12:22:47 +0100

    Added glava

---
+++ bin/run_glava.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+display_x=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
+display_y=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
+
+glava "--request=setgeometry 0 0 $display_x $display_y" "--request=setopacity xroot" -d

9c2af2d55cba7031e5fdafc9f8ac2f718d9fa263
Federico Galatolo <galatolo.federico@gmail.com>
2022-02-06 16:31:28 +0100

    fix glava random crashes

--- bin/run_glava.sh
+++ bin/run_glava.sh
@@ -5,4 +5,9 @@ sleep 3;
 display_x=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
 display_y=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
 
-glava "--request=setgeometry 0 0 $display_x $display_y" "--request=setopacity xroot" -d
+while true; do
+    glava "--request=setgeometry 0 0 $display_x $display_y" "--request=setopacity xroot" -d;
+    notify-send "Glava crashed waiting 5 seconds";
+    sleep 5;
+done
+    
